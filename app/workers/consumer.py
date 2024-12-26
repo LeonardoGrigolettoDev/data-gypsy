51327942e930266cb0ffaefdc225477d180b6d8a
LeonardoGrigolettoDev <leonardochawder@gmail.com>
2024-12-26 20:27:42 -0300

    Correção no start da função dos consumidores kafka

--- app/workers/consumer.py
+++ app/workers/consumer.py
@@ -1,6 +1,6 @@
 from kafka import KafkaConsumer
 import json
-from app.models.xml_parser import process_xml
+from app.services.xml_parser import process_xml
 
 KAFKA_TOPIC = 'xml-files-topic'
 KAFKA_SERVER = 'localhost:9093'
